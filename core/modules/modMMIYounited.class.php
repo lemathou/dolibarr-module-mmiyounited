e58b0fdd77cf236642050f75e812216205772878
Mathieu Moulin <mathieu@iprospective.fr>
2025-05-06 16:39:01 +0200

    Remove useless default menu entry

--- core/modules/modMMIYounited.class.php
+++ core/modules/modMMIYounited.class.php
@@ -305,19 +305,6 @@ class modMMIYounited extends DolibarrModules
 		// Add here entries to declare new menus
 		/* BEGIN MODULEBUILDER TOPMENU */
 		$this->menu[$r++] = array(
-			'fk_menu'=>'', // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-			'type'=>'top', // This is a Top menu entry
-			'titre'=>'ModuleMMIYounitedName',
-			'prefix' => img_picto('', $this->picto, 'class="pictofixedwidth valignmiddle"'),
-			'mainmenu'=>'mmiyounited',
-			'leftmenu'=>'',
-			'url'=>'/mmiyounited/mmiyounitedindex.php',
-			'langs'=>'mmiyounited@mmiyounited', // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-			'position'=>1000 + $r,
-			'enabled'=>'isModEnabled("mmiyounited")', // Define condition to show or hide menu entry. Use 'isModEnabled("mmiyounited")' if entry must be visible if module is enabled.
-			'perms'=>'1', // Use 'perms'=>'$user->hasRight("mmiyounited", "myobject", "read")' if you want your menu with a permission rules
-			'target'=>'',
-			'user'=>2, // 0=Menu for internal users, 1=external users, 2=both
 		);
 		/* END MODULEBUILDER TOPMENU */
 		/* BEGIN MODULEBUILDER LEFTMENU MYOBJECT */
